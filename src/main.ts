cbcd0add6d7f4fbbd0a3e1af3bf6ad4b01786c9b
Raj-StepSecurity <rajkumar@stepsecurity.io>
2025-08-20 12:44:10 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -238,7 +238,7 @@ async function validateSubscription(): Promise<void> {
     const API_URL = `https://agent.api.stepsecurity.io/v1/github/${process.env.GITHUB_REPOSITORY}/actions/subscription`;
 
     try {
-        await axios.get(API_URL, { timeout: 3000 });
+        await axios.get(API_URL, {timeout: 3000});
     } catch (error) {
         if (isAxiosError(error) && error.response) {
             core.error(
